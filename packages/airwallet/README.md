e17e472af2ca78b09c35bc3a74d7f3dc77b748dd
Akshata <akshatamohanty@gmail.com>
2022-05-04 21:38:06 +0800

    [Feature] KMS v1.0.0

--- packages/airwallet/README.md
+++ packages/airwallet/README.md
@@ -9,3 +9,11 @@ const wallets = require('wallets');
 
 // TODO: DEMONSTRATE API
 ```
+
+## Development
+
+To publish this package,
+
+1. Update `package.json` with the version number.
+2. Run `npm run build`.
+3. Run `npm publish --access public`
--- packages/airwallet/src/index.js
+++ packages/airwallet/src/index.js
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 
 import { MetaphiWalletButton } from "./MetaphiWalletButton";
 import Metaphi from "./Metaphi";
+import MetaphiWalletApi from "./MetaphiWalletApi";
 
 export default Metaphi;
-export { MetaphiWalletButton };
+export { MetaphiWalletButton, MetaphiWalletApi };
